3fd343d5f7578cbade8debd20b3d7d4a0f9fec4a
juan-ramirez <Juan@Juan-PC>
2013-03-15 16:11:30 -0500

    Cambios y refactorización de los archivos de Login, SelectNewOldFinca y NewFinca, tanto gráficos como las actividades de java.

---
+++ SWAgri/src/com/example/swagri/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.swagri;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class LoginActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+
+}
