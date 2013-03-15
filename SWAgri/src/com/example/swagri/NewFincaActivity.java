11fecd2422c4d217a1e6a59e4b994cdd1ca890e4
juan-ramirez <Juan@Juan-PC>
2013-03-15 16:13:20 -0500

    Actividades SelectNewOldFinca y NewFinca

---
+++ SWAgri/src/com/example/swagri/NewFincaActivity.java
@@ -0,0 +1,15 @@
+package com.example.swagri;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class NewFincaActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_new_finca);
+	}
+
+}
---
+++ SWAgri/src/com/example/swagri/SelectNewOldFincaActivity.java
@@ -0,0 +1,15 @@
+package com.example.swagri;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SelectNewOldFincaActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_select_new_old_finca);
+	}
+
+}
