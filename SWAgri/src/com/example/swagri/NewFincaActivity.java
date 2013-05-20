0dc50afbedeaca79396799c751a00e6c1b19f471
Mixer <mixergutierrez@gmail.com>
2013-05-19 23:29:09 -0500

    Validaciones del formulario hechas (no probadas)

--- SWAgri/src/com/example/swagri/NewFincaActivity.java
+++ SWAgri/src/com/example/swagri/NewFincaActivity.java
@@ -3,13 +3,79 @@ package com.example.swagri;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class NewFincaActivity extends Activity {
+	
+	private EditText etNombreFinca;
+	private EditText etCodigoFinca;
+	private EditText etMatriculaInmobiliaria;
+	private EditText etDireccionFinca;
+	private EditText etTelefonoFinca;
+	private EditText etExtensionFinca;
+	private EditText etNumeroLotes;
+	private Button btnCrear;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_finca);
+		
+		assignEditTexts();
+		
+		btnCrear = (Button) findViewById(R.id.btnCrearFinca);
+		btnCrear.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				if(Validate()){
+					submitForm();
+				}else{
+					Toast.makeText(NewFincaActivity.this, "Existen errores en el formulario", Toast.LENGTH_LONG).show();
+				}
+			}
+		});
 	}
+	
+	private void assignEditTexts(){
+		etNombreFinca = (EditText) findViewById(R.id.etNombreFinca);
+		etCodigoFinca = (EditText) findViewById(R.id.etCodigoFinca);
+		etMatriculaInmobiliaria = (EditText) findViewById(R.id.etMatriculaInmobiliaria);
+		etDireccionFinca = (EditText) findViewById(R.id.etDireccionFinca);
+		etTelefonoFinca = (EditText) findViewById(R.id.etTelefonoFinca);
+		etExtensionFinca = (EditText) findViewById(R.id.etExtensionFinca);
+		etNumeroLotes = (EditText) findViewById(R.id.etNumeroLotes);
+	}
+	
+	private boolean Validate(){
+		boolean ret = true;
+		
+		if(!ValidationHelper.isValid(etNombreFinca, 45, "El nombre de la finca no es válido", true)) ret = false;
+		if(!ValidationHelper.isCodigoFinca(etCodigoFinca, true)) ret = false;
+		if(!ValidationHelper.isMatriculaInmobiliaria(etMatriculaInmobiliaria, true)) ret = false;
+		if(!ValidationHelper.isValid(etDireccionFinca, 45, "La dirección de la finca no es válida", true)) ret = false;
+		if(!ValidationHelper.isPhoneNumber(etTelefonoFinca, true)) ret = false;
+		if(!ValidationHelper.isValid(etExtensionFinca, 8, "Hay un error en la extensión de la finca", true)) ret = false;
+		if(!ValidationHelper.isValid(etNumeroLotes, 10, "Hay un error en el número de lotes de la finca", true)) ret = false;
+		
+		
+		return ret;
+	}
+	
+	private void submitForm(){
+		String dataArray[] =  { etNombreFinca.getText().toString(), etCodigoFinca.getText().toString(), 
+								etMatriculaInmobiliaria.getText().toString(), etDireccionFinca.getText().toString(), 
+								etTelefonoFinca.getText().toString(), etExtensionFinca.getText().toString(), 
+								etNumeroLotes.getText().toString() };
+		
+		Toast.makeText(this, "Creando finca", Toast.LENGTH_LONG).show();
+		
+	}
+	
+	
 
 }
---
+++ SWAgri/src/com/example/swagri/ValidationHelper.java
@@ -0,0 +1,96 @@
+package com.example.swagri;
+import android.widget.EditText;
+import java.util.regex.Pattern;
+
+
+public class ValidationHelper {
+	
+	//Codigo goliado de otra parte (en parte)
+	 // Regular Expression
+    // you can change the expression based on your need
+    //private static final String EMAIL_REGEX = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    
+	private static final String PHONE_REGEX = "\\d{7}";
+    private static final String CODIGOFINCA_REGEX = "\\d{10}";
+    private static final String MATRICULAINMOBILIARIA_REGEX = "\\w{10}";
+
+    
+    // Error Messages
+    private static final String REQUIRED_MSG = "requerido";
+    private static final String PHONE_MSG = "Debe ser un teléfono de 7 dígitos, seguidos";
+    private static final String CODIGOFINCA_MSG ="Debe ser un código de 10 dígitos";
+    private static final String MATRICULAINMOBILIARIA_MSG = "Debe ser un código de 10 dígitos";
+ 
+ 
+    // call this method when you need to check phone number validation
+    public static boolean isPhoneNumber(EditText editText, boolean required) {
+        return isValid(editText, PHONE_REGEX, PHONE_MSG, required);
+    }
+    
+    //this method checks finca's code is right
+    public static boolean isCodigoFinca(EditText editText, boolean required) {
+        return isValid(editText, CODIGOFINCA_REGEX, CODIGOFINCA_MSG, required);
+    }
+
+    //same check for matricula inmobiliaria
+    public static boolean isMatriculaInmobiliaria(EditText editText, boolean required) {
+        return isValid(editText, MATRICULAINMOBILIARIA_REGEX, MATRICULAINMOBILIARIA_MSG, required);
+    }
+ 
+    // return true if the input field is valid, based on the parameter passed, when Regex are needed
+    public static boolean isValid(EditText editText, String regex, String errMsg, boolean required) {
+ 
+        String text = editText.getText().toString().trim();
+        // clearing the error, if it was previously set by some other values
+        editText.setError(null);
+ 
+        // text required and editText is blank, so return false
+        if ( required && !hasText(editText) ) return false;
+ 
+        // pattern doesn't match so returning false
+        if (required && !Pattern.matches(regex, text)) {
+            editText.setError(errMsg);
+            return false;
+        };
+ 
+        return true;
+    }
+    
+ // return true if the input field is valid, based on the parameter passed
+    public static boolean isValid(EditText editText, int numberCharacters, String errMsg, boolean required) {
+ 
+        // clearing the error, if it was previously set by some other values
+        editText.setError(null);
+ 
+        // text required and editText is blank, so return false
+        if ( required && !hasText(editText) ) return false;
+ 
+        // pattern doesn't match so returning false
+        if (editText.getText().toString().length() >= numberCharacters) {
+            editText.setError(errMsg);
+            return false;
+        };
+ 
+        return true;
+    }
+    
+ // check the input field has any text or not
+    // return true if it contains text otherwise false
+    public static boolean hasText(EditText editText) {
+ 
+        String text = editText.getText().toString().trim();
+        editText.setError(null);
+ 
+        // length 0 means there is no text
+        if (text.length() == 0) {
+            editText.setError(REQUIRED_MSG);
+            return false;
+        }
+ 
+        return true;
+    }
+
+    
+
+
+}
